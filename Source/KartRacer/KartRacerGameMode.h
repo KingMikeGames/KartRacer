3492acf7ced361d59eb7b1963d39bdfdd4717207
King <tylerklimke@gmail.com>
2016-01-22 10:23:38 -0500

    InitialCommit

---
+++ Source/KartRacer/KartRacerGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KartRacerGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KARTRACER_API AKartRacerGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
